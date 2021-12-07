ba79046273c2efe78097dd7c967153aabc1d4d45
Wajdi923 <wajdi.gdr@gmail.com>
2021-12-07 03:55:55 +0100

    Database

--- src/main/java/fr/lernejo/todo/TodoListController.java
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -7,21 +7,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
 public class TodoListController {
-    private final static List<Todo> todos = new ArrayList<Todo>();
+    private TodoRepository repository;
+
+    public TodoListController(TodoRepository repository) {
+        this.repository = repository;
+    }
 
     @GetMapping("/api/todo")
-    public ResponseEntity<List<Todo>> getTodos() {
-        return new ResponseEntity<List<Todo>>(todos, HttpStatus.OK);
+    public ResponseEntity<Iterable<TodoEntity>> getTodos() {
+        return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
     }
 
     @PostMapping(value = "/api/todo", consumes = {"application/json"})
-    public ResponseEntity<Todo> addTodo(@RequestBody Todo todo) {
-        todos.add(todo);
-        return new ResponseEntity<Todo>(todo, HttpStatus.CREATED);
+    public ResponseEntity<TodoEntity> addTodo(@RequestBody Todo todo) {
+        TodoEntity todoEntity = new TodoEntity();
+        todoEntity.message = todo.getMessage();
+        todoEntity.author = todo.getAuthor();
+        return new ResponseEntity<TodoEntity>(repository.save(todoEntity), HttpStatus.CREATED);
     }
 }
